1782e56fff8ee5d67edaed7aab4e8a2fabf39332
wwwnsai <65011588@kmitl.ac.th>
2024-10-13 23:41:27 +0700

    added update billings connects usages

--- backend/billings/src/index.ts
+++ backend/billings/src/index.ts
@@ -89,41 +89,6 @@ app.get('/billings', async (req: Request, res: Response) => {
   }
 });
 
-//get billing by user
-app.get('/billings/:user_id', async (req: Request, res: Response) => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    res.status(401).json({ error: 'Token not provided or invalid' });
-    return;
-  }
-
-  const token = authHeader.split(' ')[1];
-
-  let decodedToken;
-  try {
-    decodedToken = jwt.verify(token, JWT_SECRET);
-  } catch (err) {
-    console.error("Token verification error:", err);
-    res.status(401).json({ error: 'Invalid or expired token' });
-  }
-
-  const user_id = (decodedToken as any).userId;
-  const current_user_id = parseInt(req.params.user_id, 10);
-
-  try {
-    const billings = await Billings.findAll({ where: { user_id: current_user_id } });
-    if (!billings) {
-      res.status(404).json({ message: 'No billings found for this user' });
-      return;
-    }
-    res.json(billings);
-  } catch (err: any) {
-    console.error("Error fetching billings:", err.message);
-    res.status(500).json({ error: err.message || 'Server error' });
-  }
-});
-
 // Add billing
 app.post('/add-billing', async (req: Request, res: Response) => {
   try {
@@ -148,7 +113,6 @@ app.post('/add-billing', async (req: Request, res: Response) => {
 
     const user_id = (decodedToken as any).userId;
     const usageServiceUrl = `http://electricity-usages:3003/get-monthly-usage`;
-    // const usageServiceUrl = `http://electricity-usages:3003/`;
 
     try {
       console.log("month:", month, "year:", year);
@@ -170,7 +134,7 @@ app.post('/add-billing', async (req: Request, res: Response) => {
       const tax = Number(price) * TAX;
       const total = Number(price) + Number(tax);
 
-      const createdAt = new Date(); // Assuming you have a `createdAt` defined
+      const createdAt = new Date();
 
       const newBilling = await Billings.create({
         user_id,
@@ -197,39 +161,78 @@ app.post('/add-billing', async (req: Request, res: Response) => {
   }
 });
 
-// Update a billing entry
+
 app.put('/update-billing/:id', async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      res.status(401).json({ error: 'Token not provided or invalid' });
+      return;
+    }
+
+    const token = authHeader.split(' ')[1];
+
     try {
       const billing = await Billings.findByPk(id);
       if (!billing) {
         res.status(404).json({ error: 'Billing not found' });
         return;
       }
+      const month = billing.month;
+      const year = billing.year;
 
-      const updatedBilling = await Billings.update({
-        month: billing.month,
-        year: billing.year,
-        usage: billing.usage,
-        price: billing.price,
-        tax: billing.tax,
-        total: billing.total,
-        updated_at: dayjs().toISOString()
-      }, {
-        where: { id }
-      });
+      const usageServiceUrl = `http://electricity-usages:3003/get-monthly-usage`;
+
+      try {
+        const usageResponse = await axios.get(usageServiceUrl, {
+          headers: { Authorization: `Bearer ${token}` },
+          params: {
+            month, year
+          }
+        });
       
-      if (!updatedBilling[0]) {
-        res.status(404).json({ error: 'Billing not found' });
+        console.log("Response from service:", usageResponse.data);
+
+        if (!usageResponse.data) {
+          res.status(404).json({ message: 'No usage data found for this user and month/year' });
+          return;
+        }
+
+        const { usage, price } = usageResponse.data;
+        const tax = Number(price) * TAX;
+        const total = Number(price) + Number(tax);
+
+        console.log("Usage:", usage, "Price:", price, "Tax:", tax, "Total:", total);
+
+        const updatedBilling = await Billings.update({
+          user_id: billing.user_id,
+          month, 
+          year,
+          usage,
+          price,
+          tax,
+          total,
+          updated_at: dayjs().toISOString()
+        }, {
+          where: { id }
+        });
+        
+        if (!updatedBilling[0]) {
+          res.status(404).json({ error: 'Billing not found' });
+          return;
+        }
+
+        res.json({ message: 'Billing updated successfully' });
+      } catch (error: any) {
+        console.error("Error fetching billing:", error.message);
+        res.status(500).json({ error: error.message || 'Server error' });
         return;
       }
-
-      res.json({ message: 'Billing updated successfully' });
     } catch (error: any) {
-      console.error("Error fetching billing:", error.message);
+      console.error("Error updating billing:", error.message);
       res.status(500).json({ error: error.message || 'Server error' });
-      return;
     }
   } catch (error: any) {
     console.error("Error updating billing:", error.message);
